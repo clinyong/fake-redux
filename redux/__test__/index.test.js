1a5b61c470ad9a41bf45faafba03f8b92d68c50e
clinyong <clinyong@gmail.com>
2018-06-25 23:10:51 +0800

    Add basic redux

--- redux/__test__/index.test.js
+++ redux/__test__/index.test.js
@@ -1,12 +1,64 @@
 const { createStore, combineReducers } = require("../src");
 
-test("Test redux flow", () => {
-  const store = createStore();
+test("Test basic redux flow", () => {
+  // actions
 
-  let counter = 0;
-  function addCounter() {
-    counter++;
+  const ADD_TODO = "ADD_TODO";
+  const DEL_TODO = "DEL_TODO";
+
+  function addTodo(text) {
+    return {
+      type: ADD_TODO,
+      text
+    };
   }
 
-  store.subscribe(addCounter);
+  function delTodo() {
+    return {
+      type: DEL_TODO
+    };
+  }
+
+  // reducers
+
+  function todos(state = [], action) {
+    switch (action.type) {
+      case ADD_TODO:
+        return state.concat(action.text);
+      case DEL_TODO:
+        return state.slice(1);
+      default:
+        return state;
+    }
+  }
+
+  // store
+
+  const store = createStore(
+    combineReducers({
+      todos
+    })
+  );
+
+  let dispatchCounts = 0;
+  function addDispatchCounts() {
+    dispatchCounts++;
+  }
+
+  const unsubscribe = store.subscribe(addDispatchCounts);
+  store.dispatch(addTodo("First todo."));
+  expect(dispatchCounts).toBe(1);
+
+  store.dispatch(addTodo("Second todo."));
+  const [todo1, todo2] = store.getState().todos;
+  expect(todo1).toBe("First todo.");
+  expect(todo2).toBe("Second todo.");
+
+  store.dispatch(delTodo());
+  expect(dispatchCounts).toBe(3);
+
+  unsubscribe();
+  store.dispatch(delTodo());
+  expect(dispatchCounts).toBe(3);
+  expect(store.getState().todos.length).toBe(0);
 });
--- redux/src/index.js
+++ redux/src/index.js
@@ -1,6 +1,70 @@
-function combineReducers() {}
+/**
+ * @param {Object} reducers
+ */
+function combineReducers(reducers) {
+  const finalReducers = Object.keys(reducers).reduce((obj, k) => {
+    const reducer = reducers[k];
+    if (typeof reducer === "function") {
+      obj[k] = reducer;
+    }
 
-function createStore() {}
+    return obj;
+  }, {});
+
+  const finalKeys = Object.keys(finalReducers);
+  return function combination(state = {}, action) {
+    return finalKeys.reduce((nextState, k) => {
+      const reducer = finalReducers[k];
+      nextState[k] = reducer(state[k], action);
+      return nextState;
+    }, {});
+  };
+}
+
+/**
+ * @param {Function} reducer
+ */
+function createStore(reducer) {
+  let isDispatching = false;
+  let currentState;
+  let listeners = [];
+
+  function dispatch(action) {
+    if (isDispatching) {
+      throw new Error("Reducers may not dispatch actions.");
+    }
+
+    try {
+      isDispatching = true;
+      currentState = reducer(currentState, action);
+    } finally {
+      isDispatching = false;
+    }
+
+    listeners.forEach(listener => listener());
+
+    return action;
+  }
+
+  function subscribe(listener) {
+    listeners.push(listener);
+
+    return function unsubscribe() {
+      const index = listeners.indexOf(listener);
+      listeners.splice(index, 1);
+    };
+  }
+
+  function getState() {
+    return currentState;
+  }
+
+  return {
+    dispatch,
+    subscribe,
+    getState
+  };
+}
 
 module.exports = {
   combineReducers,
